a52c3c90d76b1e6112de035945de4203afdd8ba1
sneha <snehathakur637@gmail.com>
2024-05-29 14:04:44 +0530

    second commit

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,7 @@ const Videos = ({videos, direction }) => {
   if(!videos?.length) return <Loader />;
 
   return (
-    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={4}>
       {videos && videos.map((item, idx) => (
           <Box key={idx}>
             {item.id.videoId && <VideoCard video={item} />}
